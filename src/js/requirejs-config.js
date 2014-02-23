41bb8619027d5c6a008f9d2c8680794edcc74d1a
Ian Walter <iankwalter@me.com>
2014-02-23 13:05:14 -0500

    Removing query since it not being used

--- src/js/requirejs-config.js
+++ src/js/requirejs-config.js
@@ -9,10 +9,6 @@ var requirejsConfig = {
       '//ajax.googleapis.com/ajax/libs/angularjs/1.2.11/angular-route.min',
       'lib/angular-route/angular-route.min'
     ],
-    'jquery': [
-      '//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min',
-      'lib/jquery/jquery.min'
-    ],
     'bootstrap': [
       '//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min',
       'lib/bootstrap/dist/js/bootstrap.min'
@@ -21,9 +17,6 @@ var requirejsConfig = {
   shim: {
     'angular': {
       exports: 'angular'
-    },
-    'jquery': {
-      exports: '$'
     },
     'angular-route': {
       deps: ['angular']
